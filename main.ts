dd351576c18eba6b20f5a1120e4215be092e6b63
Mats Roshauw <karmats@gmail.com>
2020-03-13 16:42:17 +0100

    Lesson 4

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
 import { Observable, fromEvent } from "rxjs";
-import { map, filter, delay, flatMap } from "rxjs/operators";
+import {
+  map,
+  filter,
+  delay,
+  flatMap,
+  retryWhen,
+  scan,
+  takeWhile
+} from "rxjs/operators";
 
 const output = document.getElementById("output");
 const button = document.getElementById("button");
@@ -10,13 +18,17 @@ function load(url: string) {
   return Observable.create(observer => {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener("load", () => {
-      const data = JSON.parse(xhr.responseText);
-      observer.next(data);
-      observer.complete();
+      if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        observer.next(data);
+        observer.complete();
+      } else {
+        observer.error(xhr.statusText);
+      }
     });
     xhr.open("GET", url);
     xhr.send();
-  });
+  }).pipe(retryWhen(retryStrategy({ attempts: 2, wait: 2000 })));
 }
 
 function renderMovies(movies: any) {
@@ -27,7 +39,20 @@ function renderMovies(movies: any) {
   });
 }
 
-click.pipe(flatMap(() => load("movies.json"))).subscribe(
+function retryStrategy({ attempts = 5, wait = 1000 }) {
+  return function(errors) {
+    return errors.pipe(
+      scan((acc, value: number) => {
+        console.log(acc, value);
+        return acc + 1;
+      }, 0),
+      takeWhile(acc => acc < attempts),
+      delay(wait)
+    );
+  };
+}
+
+click.pipe(flatMap(() => load("moviess.json"))).subscribe(
   renderMovies,
   e => console.log(`error: ${e}`),
   () => console.log("complete")
